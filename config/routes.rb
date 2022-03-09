6f77157134e7a321298f8892c985457db1e8030d
Jan Kowalski <yakov85@o2.pl>
2022-03-09 11:36:22 +0100

    Create Article

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-
   root "articles#index"
 
-  Rails.application.routes.draw do
-    get "/articles", to: "articles#index"
-  end
+  resources :articles
 
 end
