9ad8029681a147ab6e977e5a80973db0c9f8112a
Jan Kowalski <yakov85@o2.pl>
2022-03-16 12:10:18 +0100

    Index

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -10,13 +10,23 @@ module Visible
   end
 
   class_methods do
+
     def public_count
       where(status: 'public').count
     end
+
+    def private_count
+      where(status: 'private').count
+    end
+
+    def archived_count
+      where(status: 'archived').count
+    end
+
   end
 
   def archived?
-    status == 'archived'
+    'archived' == status
   end
 
 end
